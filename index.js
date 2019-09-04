ee6134e5eeca8ecf72f5280b75a17f749e7048b3
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-04 15:02:15 -0700

    logger func

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+console.log('index.js');
+
+const server = require('./server.js');
+
+server.listen(9999, () => console.log('API'));
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log('method:', req.method);
+  console.log('url:', req.url);
+  console.log('timestamp:', Date.now());
+  next();
 };
 
 module.exports = server;
